a58fd589ef75ccd971abd2d75f763235b7b5ce6d
Vahid Nikoonejad Fard <39499671+VNF1981@users.noreply.github.com>
2019-07-08 21:03:33 +0430

    Add files via upload

---
+++ 2. Running the whole program.R
@@ -0,0 +1,35 @@
+rm(list=ls(all=TRUE))
+ls()
+
+setwd("e:/Genetics/1397 research materials/Codes for GitHub")
+getwd()
+source("e:/Genetics/1397 research materials/Codes for GitHub/1912.R")
+
+Output <- Valpop (totqtl = 20 ,
+               chrn = 2 ,
+               lenchr = 1 ,
+               totsnp = 100 ,
+               Basenum = 100 ,
+               G = 50 ,
+               Hgen = 3 ,
+               nRef = 300 ,
+               h2 = 0.1,
+               Valgen = 7 ,
+               malesintensity = 0.02 ,
+               femalesintensity = 0.2,
+               Mating.Method = "Random.Mating")
+
+attach(Output)
+names(Output)
+
+LDs
+accmat
+accmatcomp
+Inbmat
+geneticimp
+Genetic.Variance
+
+rm(list=ls(all=TRUE))
+ls()
+
+
